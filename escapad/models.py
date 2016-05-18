137b6cdade9f8e64f0b568a25c1654ab744d0d48
freddylimpens <freddy.limpens@gmail.com>
2016-05-18 18:06:39 +0200

    created Django structure and webapp with respository model

---
+++ escapad/models.py
@@ -0,0 +1,47 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Repository(models.Model):
+    
+    def set_name(self, url):
+        try:
+            name = url.strip('/').rsplit('/',1)[-1].strip('.git')
+        except Exception as e:
+            name = "default_name"
+        return name
+    
+    def set_user(self, url):
+        try:
+            user = url.strip('/').rsplit('/', 2)[-2]
+        except Exception as e:
+            user = "default_user"
+        return user
+    
+    def set_provider(self, url):
+        try:
+            provider = url.strip('/').rsplit('/', 3)[-3]
+        except Exception as e:
+            provider = "http://github.com"
+        return provider
+    
+    
+    def save(self, *args, **kwargs):
+        if not self.git_name:
+            self.git_name = self.set_name(self.git_url)
+        if not self.git_username:
+            self.git_username = self.set_user(self.git_url)
+        if not self.provider:
+            self.provider = self.set_provider(self.git_url)
+        super(Repository, self).save(*args, **kwargs)
+
+    git_url = models.URLField(max_length=200, unique=True)
+    git_name = models.CharField(max_length=200, blank=True, null=True)
+    git_username = models.CharField(max_length=200, blank=True, null=True)
+    last_compiled = models.DateTimeField(blank=True, null=True)
+    provider = models.URLField(max_length=200, blank=True, null=True)
+
+
+    
