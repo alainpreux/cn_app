94f033416dea6a9b68b09dd8d60847bb762bdf06
freddylimpens <freddy.limpens@gmail.com>
2016-06-20 15:39:13 +0200

    added toggle for home collapsed text + teaser hidden with style rather then jinja2 truncate

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,17 +1,20 @@
+// compute window height 
 function windowSize() {
     var windowHeight = $(window).height() - $('#menutop').height();
     $('.section').css({
         minHeight: windowHeight
     })
 }
-
+// reset dynamically window's size
 $(window).resize(function() {
     windowSize()
 });
-$(document).ready(function() {
-    windowSize();
-    $('#header a').click(function() {
 
+$(document).ready(function() {
+    // set min height
+    windowSize();
+    // mini page menu
+    $('#header a').click(function() {
         $('.nav li').removeClass('active');
         $(this).parent('li').addClass('active');
         var pageId = $(this).attr('href');
@@ -20,13 +23,17 @@ $(document).ready(function() {
         }, 500);
         return false
     })
-
-    var scrollPos = $('.content #page2').offset().top();
-    $('#page2').html(scrollPos);
+    // "voir plus" collapse-like buttons
+    $('body').on('show.bs.collapse', function () {
+        $('.in').collapse('hide');
+    });
+    // ?
+    // var scrollPos = $('.content #page2').offset().top();
+    // $('#page2').html(scrollPos);
 
 })
-$(window).scroll(function() {
-    var scrollCount = $(window).scrollTop();
-    $('#test').html(scrollCount);
-
-})
+// $(window).scroll(function() {
+//     var scrollCount = $(window).scrollTop();
+//     $('#test').html(scrollCount);
+// 
+// })
